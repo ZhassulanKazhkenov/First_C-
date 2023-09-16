1e6901cffe9602a36b1379067c7010714ad52ef2
Zhassulan Kazhkenov <zh.kazhkenov@gmail.com>
2023-09-16 20:15:56 +0600

    Changed the code and commented

--- HelloApp/HelloApp/Program.cs
+++ HelloApp/HelloApp/Program.cs
@@ -1,4 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Введите свое имя: ");
-var name = Console.ReadLine();
-Console.WriteLine($"Привет {name}");
+/*
+    Первая программа на C#,
+    которая выводит приветствие на консоль
+ */
+{
+    Console.WriteLine("Привет");    // Вывод строку "Привет"
+    Console.WriteLine("Добро пожаловать в С#");     // Выводим строку "Добро пожаловать в C#"
+}
