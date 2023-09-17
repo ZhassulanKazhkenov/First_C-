a79f4dd2f3d8bdebf74eca7fe31aebea97da1ed3
Zhassulan Kazhkenov <zh.kazhkenov@gmail.com>
2023-09-17 11:54:50 +0600

    Working with the console

--- HelloApp/HelloApp/Program.cs
+++ HelloApp/HelloApp/Program.cs
@@ -1,9 +1,44 @@
 ﻿// See https://aka.ms/new-console-template for more information
 /*
-    Первая программа на C#,
-    которая выводит приветствие на консоль
+    Работа с консольем
  */
+/*
 {
-    Console.WriteLine("Привет");    // Вывод строку "Привет"
-    Console.WriteLine("Добро пожаловать в С#");     // Выводим строку "Добро пожаловать в C#"
+    string hello = "Привет мир";
+    Console.WriteLine(hello);
+    Console.WriteLine("Добро пожаловать в C#!");
+    Console.WriteLine("Пока мир...");
+    Console.WriteLine(24.5);
+}
+{
+    string name = "Tom";
+    int age = 34;
+    double heigth = 1.7;
+    Console.WriteLine($"Имя: {name} Возраст: {age} Рост: {heigth}m");
+
+    Console.WriteLine("Имя: {0} Возраст: {2} Рост: {1}m", name, heigth, age);
+    
+    Console.Write($"Имя: {name} Возраст: {age} Рост: {heigth}m");
+}
+
+{
+    Console.Write("Введите свое имя: ");
+    string? name = Console.ReadLine();
+    Console.WriteLine($"Привет {name}");
+}
+*/
+{
+    Console.Write("Введите имя: ");
+    string? name = Console.ReadLine();
+
+    Console.Write("Введите возраст: ");
+    int age = Convert.ToInt32(Console.ReadLine());
+
+    Console.Write("Введите рост: ");
+    double heigth = Convert.ToDouble(Console.ReadLine());
+
+    Console.Write("Введите размер зарплаты: ");
+    decimal salary = Convert.ToDecimal(Console.ReadLine());
+
+    Console.WriteLine($"Имя: {name} Возраст: {age} Рост: {heigth}м Зарплата: {salary}$");
 }
