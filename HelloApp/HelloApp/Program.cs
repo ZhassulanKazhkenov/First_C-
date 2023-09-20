0a3a27a2a4fba7d3727a465eeed230dbc9888067
Zhassulan Kazhkenov <zh.kazhkenov@gmail.com>
2023-09-20 22:01:48 +0600

    Arithmetic operations

--- HelloApp/HelloApp/Program.cs
+++ HelloApp/HelloApp/Program.cs
@@ -1,44 +1,36 @@
 ﻿// See https://aka.ms/new-console-template for more information
-/*
-    Работа с консольем
- */
-/*
 {
-    string hello = "Привет мир";
-    Console.WriteLine(hello);
-    Console.WriteLine("Добро пожаловать в C#!");
-    Console.WriteLine("Пока мир...");
-    Console.WriteLine(24.5);
+    int x1 = 5;
+    int z1 = ++x1;
+    Console.WriteLine($"{x1} - {z1}");
 }
 {
-    string name = "Tom";
-    int age = 34;
-    double heigth = 1.7;
-    Console.WriteLine($"Имя: {name} Возраст: {age} Рост: {heigth}m");
-
-    Console.WriteLine("Имя: {0} Возраст: {2} Рост: {1}m", name, heigth, age);
-    
-    Console.Write($"Имя: {name} Возраст: {age} Рост: {heigth}m");
+    int x2 = 5;
+    int z2 = x2++;
+    Console.WriteLine($"{x2} - {z2}");
+}
+{
+    int x1 = 5;
+    int z1 = --x1;
+    Console.WriteLine($"{x1} - {z1}");
+}
+{
+    int x2 = 5;
+    int z2 = x2--;
+    Console.WriteLine($"{x2} - {z2}");
 }
 
 {
-    Console.Write("Введите свое имя: ");
-    string? name = Console.ReadLine();
-    Console.WriteLine($"Привет {name}");
+    int a = 3;
+    int b = 5;
+    int c = 40;
+    int d = c---b * a;
+    Console.WriteLine($"a={a} b={b} c={c} d={d}");
 }
-*/
 {
-    Console.Write("Введите имя: ");
-    string? name = Console.ReadLine();
-
-    Console.Write("Введите возраст: ");
-    int age = Convert.ToInt32(Console.ReadLine());
-
-    Console.Write("Введите рост: ");
-    double heigth = Convert.ToDouble(Console.ReadLine());
-
-    Console.Write("Введите размер зарплаты: ");
-    decimal salary = Convert.ToDecimal(Console.ReadLine());
-
-    Console.WriteLine($"Имя: {name} Возраст: {age} Рост: {heigth}м Зарплата: {salary}$");
+    int a = 3;
+    int b = 5;
+    int c = 40;
+    int d = (c-(--b)) * a;
+    Console.WriteLine($"a={a} b={b} c={c} d={d}");
 }
