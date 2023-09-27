d093405caa06cd7d9f71a49eee05c405a16dd2de
Zhassulan Kazhkenov <zh.kazhkenov@gmail.com>
2023-09-27 22:19:02 +0600

    Conditional expressions

--- HelloApp/HelloApp/Program.cs
+++ HelloApp/HelloApp/Program.cs
@@ -1,7 +1,66 @@
 ﻿// See https://aka.ms/new-console-template for more information
+/*
+ { 
+    Console.Write("Введите число x:");
+    int x = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Введите число y:");
+    int y = Convert.ToInt32(Console.ReadLine());
+    if (x > y)
+    {
+        Console.WriteLine($"Число {x} больше числа {y}");
+    }
+    else if (x < y)
+    {
+        Console.WriteLine($"Число {x} меньше числа {y}");
+    }
+    else
+    {
+        Console.WriteLine("Число x равно числу y");
+    }
+}
+*/
+/*
 {
-    int a = 33;
-    int b = 600;
-    byte c = (byte)(a + b);
-    Console.WriteLine(c);
+    Console.Write("Введите число x:");
+    int x = Convert.ToInt32(Console.ReadLine());
+    if (x > 5 & x < 10)
+    {
+        Console.WriteLine($"Число больше 5 и меньше 10");
+    }
+    else
+    {
+        Console.WriteLine("Неизвестное число");
+    }
+}
+*/
+/*
+{
+    Console.Write("Введите число x:");
+    int x = Convert.ToInt32(Console.ReadLine());
+    if (x == 5 || x == 10)
+    {
+        Console.WriteLine($"Число либо равно 5, либо равно 10");
+    }
+    else
+    {
+        Console.WriteLine("Неизвестное число");
+    }
+}
+*/
+{
+    Console.Write("Введите сумму вклада:");
+    double x = Convert.ToDouble(Console.ReadLine());
+    if (x < 100)
+    {
+        x += x * 0.05;
+    }
+    else if (x <= 200)
+    {
+        x += x * 0.07;
+    }
+    else
+    {
+        x += x * 0.1;
+    }
+    Console.WriteLine($"Сумму вклада после начисления процентов: {x+15}");
 }
