69949404d5b9d08b422632eff450895319947eb1
Zhassulan Kazhkenov <zh.kazhkenov@gmail.com>
2023-09-30 02:21:17 +0600

    Array

--- HelloApp/HelloApp/Program.cs
+++ HelloApp/HelloApp/Program.cs
@@ -1,71 +1,60 @@
 ﻿/*
-for (var i = 1; i<4; i++)
+int[] numbers = { 1, 2, 3, 4, 5 };
+foreach (int i in numbers)
 {
     Console.WriteLine(i);
 }
 */
-/*
-for (var i = 1; i < 4; i++)
-    Console.WriteLine(i);
-*/
-/*
-for (var i = 1; i < 4; i++) Console.WriteLine(i);
-*/
-/*
-var i = 1;
 
-for (Console.WriteLine("Начало выполнения цикла") ; i < 4; Console.WriteLine($"i={i}"))
+/*
+int[] numbers = { 1, 2, 3, 4, 5 };
+for (int i = 0; i<numbers.Length; i++)
 {
+    Console.WriteLine(numbers[i]);
+}
+*/
+/*
+int[] numbers = { 1, 2, 3, 4, 5 };
+for (int i = 0; i < numbers.Length; i++)
+{
+        numbers[i] = numbers[i] * 2;
+    Console.WriteLine(numbers[i]);
+}
+*/
+/*
+int[] numbers = { 1, 2, 3, 4, 5 };
+int i = 0;
+while(i < numbers.Length)
+{
+    Console.WriteLine(numbers[i]);
     i++;
 }
 */
 /*
-int i = 6;
+int[,] numbers = { { 1, 2, 3 }, { 4, 5, 6 } };
 
-do
+int rows = numbers.GetUpperBound(0)+1;
+int columns = numbers.GetUpperBound(1)+1;
+for (int i = 0; i < rows; i++)
 {
-    Console.WriteLine(i);
-    i--;
-}
-while (i > 0);
-*/
-/*
-int i = 6;
-
-while (i > 0)
-{
-    Console.WriteLine(i);
-    i--;
-}
-*/
-/*
-foreach(var c in "Tom")
-{
-    Console.WriteLine(c);
-}
-*/
-/*
-for (int i=0; i<9; i++)
-{
-    if (i == 5)
-        break;
-    Console.WriteLine(i);
-}
-*/
-/*
-for (int i = 0; i < 9; i++)
-{
-    if (i == 5)
-        continue;
-    Console.WriteLine(i);
-}
-*/
-
-for (int i=1; i<10; i++)
-{
-    for (int j=1; j<10; j++)
+    for(int j = 0; j < columns; j++)
     {
-        Console.WriteLine($"{i + j} \t");
+        Console.Write($"{numbers[i, j]} \t");
+    }
+    Console.WriteLine();
+}
+*/
+
+int[][] numbers = new int[3][];
+numbers[0]=new int[] {1,2};
+numbers[1] = new int[] { 1, 2, 3 };
+numbers[2] = new int[] { 1, 2, 3, 4, 5 };
+
+for (int i = 0; i < numbers.Length; i++)
+{
+    for (int j = 0; j < numbers[i].Length; j++)
+    {
+        Console.Write($"{numbers[i][j]} \t");
     }
     Console.WriteLine();
 }
