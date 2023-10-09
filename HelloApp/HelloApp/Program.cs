9e80113895c0fb0bdc56c0b520381f01f88bc79f
Zhassulan Kazhkenov <zh.kazhkenov@gmail.com>
2023-10-09 20:47:48 +0600

    Enum enumerations

--- HelloApp/HelloApp/Program.cs
+++ HelloApp/HelloApp/Program.cs
@@ -1,76 +1,22 @@
-﻿string name = "Alex";
-switch (name)
+﻿DoOperation(10, 5, Operation.Add);
+DoOperation(10, 5, Operation.Subtract);
+DoOperation(10, 5, Operation.Multiply);
+DoOperation(10, 5, Operation.Divide);
+void DoOperation(double x, double y, Operation op)
 {
-    case "Tom":
-        Console.WriteLine("Ваше имя - Том");
-        break;
-    case "Zhas":
-        Console.WriteLine("Ваше имя - Zhas");
-        break;
-    case "John":
-        Console.WriteLine("Ваше имя - Том");
-        break;
-    default:
-        Console.WriteLine("Неизвестное имя");
-        break;
-}
-
-int number = 1;
-switch (number)
-{
-    case 1:
-        Console.WriteLine("case 1");
-        goto case 5;
-    case 3:
-        Console.WriteLine("case 3");
-        break;
-    case 5:
-        Console.WriteLine("case 5");
-        break;
-    default:
-        Console.WriteLine("default");
-        break;
-}
-/*
-int DoOperation(int op, int a, int b)
-{
-    switch (op)
+    double result = op switch
     {
-        case 1: return a + b;
-        case 2: return a - b;
-        case 3: return a * b;
-        default: return 0;
-    }
-}
-*/
-int result1 = DoOperation(1, 10, 5);
-Console.WriteLine(result1);
-
-int result2 = DoOperation(4, 10, 5);
-Console.WriteLine(result2);
-
-int DoOperation(int op, int a, int b)
-{
-    int result = op switch
-    {
-        1 => a + b,
-        2 => a - b,
-        3 => a * b,
-        _ => 0
+        Operation.Add => x + y,
+        Operation.Subtract => x - y,
+        Operation.Multiply => x * y,
+        Operation.Divide => x / y
     };
-    return result;
+    Console.WriteLine(result);
 }
-
-int DoOperation1(int op, int a, int b) => op switch
+enum Operation
 {
-        1 => a + b,
-        2 => a - b,
-        3 => a * b,
-        _ => 0
-};
-
-int result3 = DoOperation1(2, 10, 5);
-Console.WriteLine(result3);
-
-int result4 = DoOperation1(4, 10, 5);
-Console.WriteLine(result4);
+    Add,
+    Subtract,
+    Multiply,
+    Divide
+}
