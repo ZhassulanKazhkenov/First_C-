e309b459baceeaf37ce290f1daa7d14f9b1f6d3e
Zhassulan Kazhkenov <zh.kazhkenov@gmail.com>
2023-10-14 12:15:01 +0600

    The Program class and the Main method. Top-level programs

--- HelloApp/HelloApp/Program.cs
+++ HelloApp/HelloApp/Program.cs
@@ -1,44 +1,31 @@
-﻿/*
+﻿// инструкции верхнего уровня (top-level statements)
 Person tom = new();
-Person bob = new("Bob");
-Person sam = new("Sam", 25);
+tom.SayHello();
 
-tom.Print();          // Имя: Неизвестно  Возраст: 18
-bob.Print();          // Имя: Bob  Возраст: 18
-sam.Print();
+// определение класса идет после инструкций верхнего уровня
+class Person
+{
+    public void SayHello() => Console.WriteLine("Hello");
+}
+/*
+Person tom = new();
+tom.SayHello();
 
 class Person
 {
-    public string name;
-    public int age;
-    public Person() : this("Неизвестно")    // первый конструктор
-    { }
-    public Person(string name) : this(name, 18) // второй конструктор
-    { }
-    public Person(string name, int age)     // третий конструктор
+    public void SayHello() => Console.WriteLine("Hello");
+}
+
+class Program
+{
+    static void Main(string[] args)
     {
-        this.name = name;
-        this.age = age;
+        Person tom = new();
+        tom.SayHello();
     }
-    public void Print() => Console.WriteLine($"Имя: {name}  Возраст: {age}");
+}
+class Person
+{
+    public void SayHello() => Console.WriteLine("Hello");
 }
 */
-Person tom = new Person { name = "Tom", company = { title = "Microsoft" } };
-tom.Print();          // Имя: Tom  Компания: Microsoft
-
-class Person
-{
-    public string name;
-    public Company company;
-    public Person()
-    {
-        name = "Undefined";
-        company = new Company();
-    }
-    public void Print() => Console.WriteLine($"Имя: {name}  Компания: {company.title}");
-}
-
-class Company
-{
-    public string title = "Unknown";
-}
