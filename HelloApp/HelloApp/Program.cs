6dbec742d61eed3420193b35658a11906a91035e
Zhassulan Kazhkenov <zh.kazhkenov@gmail.com>
2023-10-15 12:33:27 +0600

    Structures

--- HelloApp/HelloApp/Program.cs
+++ HelloApp/HelloApp/Program.cs
@@ -1,31 +1,65 @@
-﻿// инструкции верхнего уровня (top-level statements)
-Person tom = new();
-tom.SayHello();
+﻿/*
+ * Person tom = new Person();
+tom.Print();
 
-// определение класса идет после инструкций верхнего уровня
-class Person
+struct Person
 {
-    public void SayHello() => Console.WriteLine("Hello");
-}
-/*
-Person tom = new();
-tom.SayHello();
-
-class Person
-{
-    public void SayHello() => Console.WriteLine("Hello");
-}
-
-class Program
-{
-    static void Main(string[] args)
-    {
-        Person tom = new();
-        tom.SayHello();
+    public string name = "Zhas";  
+    public int age = 1;
+    public Person() { } //Вызов конструктора по умолчанию
+    public void Print()=> Console.WriteLine($"Имя: {name} Возраст: {age}");
+    /*{
+        Console.WriteLine($"Имя: {name} Возраст: {age}");
     }
 }
-class Person
+*/
+/*
+Person tom = new Person();
+tom.Print();
+struct Person
 {
-    public void SayHello() => Console.WriteLine("Hello");
+    public string name;
+    public int age;
+
+    public Person() 
+    {
+        name = "Tom";
+        age = 37;
+    }
+    public void Print() => Console.WriteLine($"Имя: {name} Возраст: {age}");
 }
 */
+/*
+Person tom = new Person();
+Person bob = new Person("Bob");
+Person sam = new Person("Sam", 25);
+
+tom.Print();
+bob.Print();
+sam.Print();
+
+struct Person
+{
+    public string name;
+    public int age;
+
+    public Person() :this("Tom")
+    { }
+    public Person(string fullName) : this(fullName, 1)
+    { }
+    public Person(string fullName, int fullAge)
+    {
+        this.name = fullName;
+        this.age = fullAge;
+    }
+    public void Print() => Console.WriteLine($"Имя: {name} Возраст: {age}");
+}
+*/
+Person tom = new Person { name = "Tom", age = 22};
+tom.Print();
+struct Person
+{
+    public string name;
+    public int age;
+    public void Print() => Console.WriteLine($"Имя: {name} Возраст: {age}");
+}
