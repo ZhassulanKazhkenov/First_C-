bc2937429f8405974e56805178814fc8e5355eab
Zhassulan Kazhkenov <zh.kazhkenov@gmail.com>
2023-10-16 23:39:22 +0600

    Value types and reference types

--- HelloApp/HelloApp/Program.cs
+++ HelloApp/HelloApp/Program.cs
@@ -1,65 +1,19 @@
-﻿/*
- * Person tom = new Person();
-tom.Print();
+﻿Person p = new Person { name = "Tom", age = 23 };
+ChangePerson(ref p);
 
-struct Person
+Console.WriteLine(p.name); // Bill
+Console.WriteLine(p.age); // 45
+
+void ChangePerson(ref Person person)
 {
-    public string name = "Zhas";  
-    public int age = 1;
-    public Person() { } //Вызов конструктора по умолчанию
-    public void Print()=> Console.WriteLine($"Имя: {name} Возраст: {age}");
-    /*{
-        Console.WriteLine($"Имя: {name} Возраст: {age}");
-    }
+    // сработает
+    person.name = "Alice";
+    // сработает
+    person = new Person { name = "Bill", age = 45 };
 }
-*/
-/*
-Person tom = new Person();
-tom.Print();
-struct Person
+
+class Person
 {
-    public string name;
+    public string name = "";
     public int age;
-
-    public Person() 
-    {
-        name = "Tom";
-        age = 37;
-    }
-    public void Print() => Console.WriteLine($"Имя: {name} Возраст: {age}");
-}
-*/
-/*
-Person tom = new Person();
-Person bob = new Person("Bob");
-Person sam = new Person("Sam", 25);
-
-tom.Print();
-bob.Print();
-sam.Print();
-
-struct Person
-{
-    public string name;
-    public int age;
-
-    public Person() :this("Tom")
-    { }
-    public Person(string fullName) : this(fullName, 1)
-    { }
-    public Person(string fullName, int fullAge)
-    {
-        this.name = fullName;
-        this.age = fullAge;
-    }
-    public void Print() => Console.WriteLine($"Имя: {name} Возраст: {age}");
-}
-*/
-Person tom = new Person { name = "Tom", age = 22};
-tom.Print();
-struct Person
-{
-    public string name;
-    public int age;
-    public void Print() => Console.WriteLine($"Имя: {name} Возраст: {age}");
 }
