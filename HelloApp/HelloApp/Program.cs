ead9768c48ae90cb58b237dcb7ea44f926976b99
Zhassulan Kazhkenov <zh.kazhkenov@gmail.com>
2023-10-17 23:44:11 +0600

    Scope (context) of variables and constants

--- HelloApp/HelloApp/Program.cs
+++ HelloApp/HelloApp/Program.cs
@@ -1,19 +1,34 @@
-﻿Person p = new Person { name = "Tom", age = 23 };
-ChangePerson(ref p);
+﻿Person tom = new();
+tom.PrintName();
+tom.PrintSurname();
 
-Console.WriteLine(p.name); // Bill
-Console.WriteLine(p.age); // 45
-
-void ChangePerson(ref Person person)
+class Person                            // начало контекста класса
 {
-    // сработает
-    person.name = "Alice";
-    // сработает
-    person = new Person { name = "Bill", age = 45 };
-}
+    string type = "Person";             // переменная уровня класса
+    public void PrintName()             // начало контекста метода PrintName
+    {
+        string name = "Tom";            // переменная уровня метода
 
-class Person
-{
-    public string name = "";
-    public int age;
-}
+        {                               // начало контекста блока кода
+            string shortName = "Tomas"; // переменная уровня блока кода
+            Console.WriteLine(type);    // в блоке доступна переменная класса
+            Console.WriteLine(name);    // в блоке доступна переменная окружающего метода
+            Console.WriteLine(shortName);// в блоке доступна переменная этого же блока
+        }                               // конец контекста блока кода, переменная shortName уничтожается
+
+        Console.WriteLine(type);        // в методе доступна переменная класса
+        Console.WriteLine(name);        // в методе доступна переменная этого же метода
+                                        //Console.WriteLine(shortName); //так нельзя, переменная c определена в блоке кода
+                                        //Console.WriteLine(surname);     //так нельзя, переменная surname определена в другом методе
+
+    }       // конец контекста метода PrintName, переменная name уничтожается
+
+    public void PrintSurname()      // начало контекста метода PrintSurname
+    {
+        string surname = "Smith";   // переменная уровня метода
+
+        Console.WriteLine(type);        // в методе доступна переменная класса
+        Console.WriteLine(surname);     // в методе доступна переменная этого же метода 
+    }       // конец конекста метода PrintSurname, переменная surname уничтожается
+
+}   // конец контекста класса, переменная type уничтожается
