fcffff89cef1d5b03083170509849c3c7bc10268
Жасулан Кажкенов <68074968+ZhassulanKazhkenov@users.noreply.github.com>
2023-10-21 11:23:40 +0600

    Method overloading

--- HelloApp/HelloApp/Program.cs
+++ HelloApp/HelloApp/Program.cs
@@ -1,4 +1,30 @@
-﻿
-Company microsoft = new("Microsoft");
-Person tom = new("Tom", microsoft);
-tom.Print();
+﻿Calculator calc = new Calculator();
+calc.Add(1, 2);
+calc.Add(1, 2, 3);
+calc.Add(1, 2, 3, 4);
+calc.Add(1.4, 2.3);
+class Calculator
+{
+    public void Add(int a, int b)
+    {
+        int result = a + b;
+        Console.WriteLine($"Result is {result}");
+    }
+    public void Add(int a, int b, int c)
+    {
+        int result = a + b + c;
+        Console.WriteLine($"Result is {result}");
+    }
+    public int Add(int a,int b,int c,int d)
+    {
+        int result = a + b + c + d;
+        Console.WriteLine($"Result is {result}");
+        return result;
+    }
+    public void Add(double a, double b)
+    {
+        double result = a + b;
+        Console.WriteLine($"Result is {result}");
+    }
+}
+
