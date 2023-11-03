ad21fdd90807cb14ca2816616e643852074e0dba
Жасулан Кажкенов <68074968+ZhassulanKazhkenov@users.noreply.github.com>
2023-11-04 01:11:18 +0600

    Abstract classes and class members

--- HelloApp/HelloApp/Program.cs
+++ HelloApp/HelloApp/Program.cs
@@ -1,31 +1,76 @@
-﻿//Различие переопределения и скрытия методов
-//Переопределение
+﻿//Абстрактные классы и члены классов
+//Абстрактные классы
+/*
+Transport car = new Car("машина");
+Transport ship = new Ship("корабль");
+Transport aircraft = new Aircraft("самолет");
 
-Person tom = new Employee("Tom", "Microsoft");
-tom.Print();        // Tom работает в Microsoft
-class Person
+car.Move();         // машина движется
+ship.Move();        // корабль движется
+aircraft.Move();    // самолет движется
+abstract class Transport
 {
-    public string Name { get; set; }
-    public Person(string name)
+    public string Name { get; }
+    // конструктор абстрактного класса Transport
+    public Transport(string name)
     {
         Name = name;
     }
-    public virtual void Print()
+    public void Move() => Console.WriteLine($"{Name} движется");
+}
+// класс корабля
+class Ship : Transport
+{
+    // вызываем конструктор базового класса
+    public Ship(string name) : base(name) { }
+}
+// класс самолета
+class Aircraft : Transport
+{
+    public Aircraft(string name) : base(name) { }
+}
+// класс машины
+class Car : Transport
+{
+    public Car(string name) : base(name) { }
+}
+*/
+//Абстрактные методы
+
+Transport car = new Car();
+Transport ship = new Ship();
+Transport aircraft = new Aircraft();
+
+car.Move();         // машина едет
+ship.Move();        // корабль плывет
+aircraft.Move();    // самолет летит
+
+abstract class Transport
+{
+    public abstract void Move();
+}
+// класс корабля
+class Ship : Transport
+{
+    // мы должны реализовать все абстрактные методы и свойства базового класса
+    public override void Move()
     {
-        Console.WriteLine(Name);
+        Console.WriteLine("Корабль плывет");
     }
 }
-class Employee : Person
+// класс самолета
+class Aircraft : Transport
 {
-    public string Company { get; set; }
-    public Employee(string name, string company)
-        : base(name)
+    public override void Move()
     {
-        Company = company;
+        Console.WriteLine("Самолет летит");
     }
-
-    public override void Print()
+}
+// класс машины
+class Car : Transport
+{
+    public override void Move()
     {
-        Console.WriteLine($"{Name} работает в {Company}");
+        Console.WriteLine("Машина едет");
     }
 }
