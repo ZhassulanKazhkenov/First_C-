c8db971effcc294510f2b080e05da7fe29d41dfe
Жасулан Кажкенов <68074968+ZhassulanKazhkenov@users.noreply.github.com>
2023-11-03 16:55:47 +0600

    The difference between overriding and hiding methods

--- HelloApp/HelloApp/Program.cs
+++ HelloApp/HelloApp/Program.cs
@@ -1,23 +1,21 @@
-﻿//Скрытие методов и свойств
-/*
-Person bob = new Person("Bob");
-bob.Print();
-Employee tom = new Employee("Tom", "Google");
+﻿//Различие переопределения и скрытия методов
+//Переопределение
+
+Person tom = new Employee("Tom", "Microsoft");
 tom.Print();
-*/
-/*
-    class Person
+class Person
 {
     public string Name { get; set; }
     public Person(string name)
     {
         Name = name;
     }
-        public void Print() 
-        {
-            Console.WriteLine($"Name: {Name}");
-        }
+    public void Print() 
+    {
+        Console.WriteLine(Name);
+    }
 }
+
 class Employee : Person
 {
     public string Company { get; set; }
@@ -25,60 +23,10 @@ class Employee : Person
     {
         Company = company;
     }
+
     public new void Print()
     {
-        Console.WriteLine($"Name: {Name} Company: {Company}");
+        Console.WriteLine($"{Name} работает в {Company}");
     }
 }
-*/
-//Скрытие свойств
-/*
-Person bob = new Person("Bob");
-Console.WriteLine(bob.Name);    // Bob
-
-Employee tom = new Employee("Tom", "Microsoft");
-Console.WriteLine(tom.Name);    // Mr./Ms. Tom
-
-class Person
-{
-    public string Name { get; set; }
-    public Person(string name)
-    {
-        Name = name;
-    }
-}
-class Employee : Person
-{
-    // скрываем свойство Name базового класса
-    public new string Name
-    {
-        get => $"Mr./Ms. {base.Name}";
-        set => base.Name = value;
-
-    }
-    public string Company { get; set; }
-    public Employee(string name, string company)
-                : base(name)
-    {
-        Company = company;
-    }
-}
-*/
-//Скрытие переменных и констант
-Console.WriteLine(Person.minAge);     // 1
-Console.WriteLine(Person.typeName);   // Person
-
-Console.WriteLine(Employee.minAge);     // 18
-Console.WriteLine(Employee.typeName);   // Employee
-
-class Person
-{
-    public readonly static int minAge = 1;
-    public const string typeName = "Person";
-}
-class Employee : Person
-{
-    // скрываем поля и константы базового класса
-    public new readonly static int minAge = 18;
-    public new const string typeName = "Employee";
-}
+//Скрытие
