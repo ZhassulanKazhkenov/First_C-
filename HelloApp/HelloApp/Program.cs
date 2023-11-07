31a23e94d1ef20cd4840e592e9442f2734a2fe7b
Жасулан Кажкенов <68074968+ZhassulanKazhkenov@users.noreply.github.com>
2023-11-07 23:23:31 +0600

    Limitations of generalizations

--- HelloApp/HelloApp/Program.cs
+++ HelloApp/HelloApp/Program.cs
@@ -1,27 +1,82 @@
-﻿//Обобщения
-// класс компании
+﻿//Limitations of generalizations
+/*
+SendMessage(new Message("Hello World"));
+SendMessage(new EmailMessage("Bye World"));
 
-Person<int> tom = new Person<int>(546, "Tom");
-Company<Person<int>> microsoft = new Company<Person<int>>(tom);
-
-Console.WriteLine(microsoft.CEO.Id);  // 546
-Console.WriteLine(microsoft.CEO.Name);  // Tom
-
-class Company<P>
+void SendMessage<T>(T message) where T : Message
 {
-    public P CEO { get; set; }  // президент компании
-    public Company(P ceo)
+    Console.WriteLine($"Отправляется сообщение: {message.Text}");
+}
+class Message
+{
+    public string Text { get; } // текст сообщения
+    public Message(string text)
     {
-        CEO = ceo;
+        Text = text;
     }
 }
-class Person<T>
+class EmailMessage : Message
 {
-    public T Id { get; }
-    public string Name { get; }
-    public Person(T id, string name)
+    public EmailMessage(string text) : base(text) { }
+}
+*/
+//Limitations of generalizations in types
+
+/*
+
+Messenger<Message> telegram = new Messenger<Message>();
+telegram.SendMessage(new Message("Hello World"));
+
+Messenger<EmailMessage> outlook = new Messenger<EmailMessage>();
+outlook.SendMessage(new EmailMessage("Bye World"));
+class Messenger<T> where T : Message
+{
+    public void SendMessage(T message)
     {
-        Id = id;
-        Name = name;
+        Console.WriteLine($"Отправляется сообщение: {message.Text}");
     }
+}
+
+class Message
+{
+    public string Text { get; } // текст сообщения
+    public Message(string text)
+    {
+        Text = text;
+    }
+}
+class EmailMessage : Message
+{
+    public EmailMessage(string text) : base(text) { }
+}
+*/
+//Using multiple universal parameters
+
+Messenger<Message, Person> telegram = new Messenger<Message, Person>();
+Person tom = new Person("Tom");
+Person bob = new Person("Bob");
+Message hello = new Message("Hello, Bob!");
+telegram.SendMessage(tom, bob, hello);
+
+class Messenger<T, P>
+    where T : Message
+    where P : Person
+{
+    public void SendMessage(P sender, P receiver, T message)
+    {
+        Console.WriteLine($"Отправитель: {sender.Name}");
+        Console.WriteLine($"Получатель: {receiver.Name}");
+        Console.WriteLine($"Сообщение: {message.Text}");
+    }
+}
+class Person
+{
+    public string Name { get; }
+    public Person(string name) => Name = name;
+}
+
+class Message
+{
+    public string Text { get; } // текст сообщения
+    public Message(string text) => Text = text;
 }
