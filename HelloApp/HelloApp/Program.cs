68bbcb29554204634b992879a45811d7c5cadcdf
Жасулан Кажкенов <68074968+ZhassulanKazhkenov@users.noreply.github.com>
2023-11-10 00:56:24 +0600

    Inheritance of generalized types

--- HelloApp/HelloApp/Program.cs
+++ HelloApp/HelloApp/Program.cs
@@ -1,82 +1,70 @@
-﻿//Limitations of generalizations
+﻿//Inheritance of generalized types
 /*
-SendMessage(new Message("Hello World"));
-SendMessage(new EmailMessage("Bye World"));
-
-void SendMessage<T>(T message) where T : Message
-{
-    Console.WriteLine($"Отправляется сообщение: {message.Text}");
-}
-class Message
-{
-    public string Text { get; } // текст сообщения
-    public Message(string text)
-    {
-        Text = text;
-    }
-}
-class EmailMessage : Message
-{
-    public EmailMessage(string text) : base(text) { }
-}
+Person<string> person1 = new Person<string>("34");
+Person<int> person3 = new UniversalPerson<int>(45);
+UniversalPerson<int> person2 = new UniversalPerson<int>(33);
+Console.WriteLine(person1.Id);
+Console.WriteLine(person2.Id);
+Console.WriteLine(person3.Id);
 */
-//Limitations of generalizations in types
+/*
+StringPerson person4 = new StringPerson("438767");
+Person<string> person5 = new StringPerson("43875");
+//Person<int> person6 = new StringPerson("2112432");
+Console.WriteLine(person4.Id);
+Console.WriteLine(person5.Id);
+*/
+
+IntPerson<string> person7 = new IntPerson<string>(5, "r4556");
+Person<int> person8 = new IntPerson<long>(7, 4567);
+Console.WriteLine(person7.Id);
+Console.WriteLine(person8.Id);
 
 /*
+MixedPerson<string, int> person9 = new MixedPerson<string, int>("456", 356);
+Person<string> person10 = new MixedPerson<string, int>("9867", 35678);
+Console.WriteLine(person9.Id);
+Console.WriteLine(person10.Id);
+*/
 
-Messenger<Message> telegram = new Messenger<Message>();
-telegram.SendMessage(new Message("Hello World"));
-
-Messenger<EmailMessage> outlook = new Messenger<EmailMessage>();
-outlook.SendMessage(new EmailMessage("Bye World"));
-class Messenger<T> where T : Message
+class Person<T>
 {
-    public void SendMessage(T message)
+    public T Id { get; }
+    public Person(T id)
     {
-        Console.WriteLine($"Отправляется сообщение: {message.Text}");
+        Id = id;
     }
 }
-
-class Message
+/*
+class UniversalPerson<T> : Person<T>
 {
-    public string Text { get; } // текст сообщения
-    public Message(string text)
-    {
-        Text = text;
-    }
-}
-class EmailMessage : Message
-{
-    public EmailMessage(string text) : base(text) { }
+    public UniversalPerson(T id) : base(id) { }
 }
 */
-//Using multiple universal parameters
-
-Messenger<Message, Person> telegram = new Messenger<Message, Person>();
-Person tom = new Person("Tom");
-Person bob = new Person("Bob");
-Message hello = new Message("Hello, Bob!");
-telegram.SendMessage(tom, bob, hello);
-
-class Messenger<T, P>
-    where T : Message
-    where P : Person
+/*
+class StringPerson : Person<string>
 {
-    public void SendMessage(P sender, P receiver, T message)
+    public StringPerson(string id) : base(id) { }
+}
+*/
+
+class IntPerson<T> : Person<int>
+{
+    public T Code { get; set; }
+    public IntPerson(int id, T code) : base(id)
     {
-        Console.WriteLine($"Отправитель: {sender.Name}");
-        Console.WriteLine($"Получатель: {receiver.Name}");
-        Console.WriteLine($"Сообщение: {message.Text}");
+        Code = code;
     }
 }
-class Person
-{
-    public string Name { get; }
-    public Person(string name) => Name = name;
-}
 
-class Message
+/*
+class MixedPerson<T, K> : Person<T>
+    where K: struct
 {
-    public string Text { get; } // текст сообщения
-    public Message(string text) => Text = text;
+    public K Code { get; set; }
+    public MixedPerson(T id, K code) : base(id)
+    {
+        Code = code;
+    }
 }
+*/
