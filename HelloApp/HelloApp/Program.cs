0a06e653e5b601edae8a90ada3c55ebe76a99755
Жасулан Кажкенов <68074968+ZhassulanKazhkenov@users.noreply.github.com>
2023-11-13 01:19:25 +0600

    Exception Handling

--- HelloApp/HelloApp/Program.cs
+++ HelloApp/HelloApp/Program.cs
@@ -1,70 +1,35 @@
-﻿//Inheritance of generalized types
+﻿//Exception Handling
+//Construction try..catch..finally
 /*
-Person<string> person1 = new Person<string>("34");
-Person<int> person3 = new UniversalPerson<int>(45);
-UniversalPerson<int> person2 = new UniversalPerson<int>(33);
-Console.WriteLine(person1.Id);
-Console.WriteLine(person2.Id);
-Console.WriteLine(person3.Id);
-*/
-/*
-StringPerson person4 = new StringPerson("438767");
-Person<string> person5 = new StringPerson("43875");
-//Person<int> person6 = new StringPerson("2112432");
-Console.WriteLine(person4.Id);
-Console.WriteLine(person5.Id);
-*/
-
-IntPerson<string> person7 = new IntPerson<string>(5, "r4556");
-Person<int> person8 = new IntPerson<long>(7, 4567);
-Console.WriteLine(person7.Id);
-Console.WriteLine(person8.Id);
-
-/*
-MixedPerson<string, int> person9 = new MixedPerson<string, int>("456", 356);
-Person<string> person10 = new MixedPerson<string, int>("9867", 35678);
-Console.WriteLine(person9.Id);
-Console.WriteLine(person10.Id);
-*/
-
-class Person<T>
+try
 {
-    public T Id { get; }
-    public Person(T id)
+    int x = 5;
+    int y = x / 0;
+    Console.WriteLine($"Результат: {y}");
+}
+catch
+{
+    Console.WriteLine("Возникло исключение!");
+}
+finally
+{
+    Console.WriteLine("Блок finally");
+}
+Console.WriteLine("Конец программы");
+*/
+//Exception handling and conditional constructions
+
+Square("12"); // Квадрат числа 12: 144
+Square("ab"); // Некорректный ввод
+
+void Square(string data)
+{
+    if (int.TryParse(data, out var x))
     {
-        Id = id;
+        Console.WriteLine($"Квадрат числа {x}: {x * x}");
+    }
+    else
+    {
+        Console.WriteLine("Некорректный ввод");
     }
 }
-/*
-class UniversalPerson<T> : Person<T>
-{
-    public UniversalPerson(T id) : base(id) { }
-}
-*/
-/*
-class StringPerson : Person<string>
-{
-    public StringPerson(string id) : base(id) { }
-}
-*/
-
-class IntPerson<T> : Person<int>
-{
-    public T Code { get; set; }
-    public IntPerson(int id, T code) : base(id)
-    {
-        Code = code;
-    }
-}
-
-/*
-class MixedPerson<T, K> : Person<T>
-    where K: struct
-{
-    public K Code { get; set; }
-    public MixedPerson(T id, K code) : base(id)
-    {
-        Code = code;
-    }
-}
-*/
