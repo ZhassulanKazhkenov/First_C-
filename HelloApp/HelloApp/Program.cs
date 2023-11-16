7bb80f83687a07f453d19d387fee2b1a3558198f
Жасулан Кажкенов <68074968+ZhassulanKazhkenov@users.noreply.github.com>
2023-11-16 22:14:00 +0600

    Delegate

--- HelloApp/HelloApp/Program.cs
+++ HelloApp/HelloApp/Program.cs
@@ -1,47 +1,27 @@
-﻿//Finding the catch block when handling exceptions
-try
-{
-    TestClass.Method1();
-}
-catch (DivideByZeroException ex)
-{
-    Console.WriteLine($"Catch в Main : {ex.Message}");
-}
-finally
-{
-    Console.WriteLine("Блок finally в Main");
-}
-Console.WriteLine("Конец метода Main");
+﻿//Делегаты
+/*
+Message mes;
+mes = Hello;
+mes();
 
-class TestClass
+void Hello() => Console.WriteLine("Hello Metanit.com");
+
+delegate void Message();
+*/
+
+Message message1 = Welcome.Print;
+Message message2 = new Hello().Display;
+
+message1();
+message2();
+
+delegate void Message();
+
+class Welcome
 {
-    public static void Method1()
-    {
-        try
-        {
-            Method2();
-        }
-        catch (IndexOutOfRangeException ex)
-        {
-            Console.WriteLine($"Catch в Method1 : {ex.Message}");
-        }
-        finally
-        {
-            Console.WriteLine("Блок finally в Method1");
-        }
-        Console.WriteLine("Конец метода Method1");
-    }
-    static void Method2()
-    {
-        try
-        {
-            int x = 8;
-            int y = x / 0;
-        }
-        finally
-        {
-            Console.WriteLine("Блок finally в Method2");
-        }
-        Console.WriteLine("Конец метода Method2");
-    }
+    public static void Print() => Console.WriteLine("Welcome");
+}
+class Hello
+{
+    public void Display() => Console.WriteLine("Привет");
 }
