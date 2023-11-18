e43e572fbf1a93cb642ebd8e8efcc117ffb3907d
Жасулан Кажкенов <68074968+ZhassulanKazhkenov@users.noreply.github.com>
2023-11-18 13:12:05 +0600

    Anonymous methods

--- HelloApp/HelloApp/Program.cs
+++ HelloApp/HelloApp/Program.cs
@@ -1,87 +1,51 @@
-﻿//Using delegates
-using System.Diagnostics;
+﻿//Anonymous methods
 /*
-void PrintSimpleMessage(string message) => Console.WriteLine(message);
-
-var account = new Account(200);
-account.RegisterHandler(PrintSimpleMessage);
-account.Take(100);
-account.Take(150);
-
-public delegate void AccountHandler(string message);
-public class Account
+MessageHandler handler = delegate (string mes)
 {
-    int sum;
-    AccountHandler? taken;
-    public Account(int sum) => this.sum = sum;
-    public void RegisterHandler(AccountHandler del)
-    {
-        taken = del;
-    }
-    public void Add(int sum) => this.sum+= sum;
-    public void Take(int sum)
-    {
-        if(this.sum >= sum)
-        {
-            this.sum -=sum;
-            taken?.Invoke($"Со счета списано {sum} y.e");
-        }
-        else
-        {
-            taken?.Invoke($"Недостаточно средств. Баланс: {this.sum} y.e.");
-        }
-    }
-}
+    Console.WriteLine(mes);
+};
+handler("Hai Hitler!!!");
+
+delegate void MessageHandler(string message);
 */
-Account account = new Account(200);
-// Добавляем в делегат ссылку на методы
-account.RegisterHandler(PrintSimpleMessage);
-account.RegisterHandler(PrintColorMessage);
-// Два раза подряд пытаемся снять деньги
-account.Take(100);
-account.Take(150);
-
-// Удаляем делегат
-account.UnregisterHandler(PrintColorMessage);
-// снова пытаемся снять деньги
-account.Take(50);
-
-void PrintSimpleMessage(string message) => Console.WriteLine(message);
-void PrintColorMessage(string message)
+/*
+ShowMessage("hai!", delegate (string mes)
 {
-    // Устанавливаем красный цвет символов
-    Console.ForegroundColor = ConsoleColor.Red;
-    Console.WriteLine(message);
-    // Сбрасываем настройки цвета
-    Console.ResetColor();
+    Console.WriteLine(mes);
+});
+
+static void ShowMessage(string message, MessageHandler handler)
+{
+    handler(message);
 }
 
-public delegate void AccountHandler(string message);
-public class Account
+delegate void MessageHandler(string message);
+*/
+/*
+MessageHandler handler = delegate
 {
-    int sum;
-    AccountHandler? taken;
-    public Account(int sum) => this.sum = sum;
-    // Регистрируем делегат
-    public void RegisterHandler(AccountHandler del)
-    {
-        taken += del;
-    }
-    // Отмена регистрации делегата
-    public void UnregisterHandler(AccountHandler del)
-    {
-        taken -= del; // удаляем делегат
-    }
-    public void Add(int sum) => this.sum += sum;
-    public void Take(int sum)
-    {
-        if (this.sum >= sum)
-        {
-            this.sum -= sum;
-            taken?.Invoke($"Со счета списано {sum} у.е.");
-        }
-        else
-            taken?.Invoke($"Недостаточно средств. Баланс: {this.sum} у.е.");
-    }
-}
+    Console.WriteLine("Слава третьему рейху!!!");
+};
+handler("hello world!");
 
+delegate void MessageHandler(string message);
+*/
+/*
+Operation operation = delegate (int x, int y)
+{
+    return x + y;
+};
+int result = operation(4, 5);
+Console.WriteLine(result);
+
+delegate int Operation(int x, int y);
+*/
+int z = 8;
+Operation operation = delegate (int x, int y)
+{
+    return z + x + y;
+};
+int result = operation(4, 5);
+Console.WriteLine(result);
+
+delegate int Operation(int x, int y);
